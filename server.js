8de458c12b0335eedb3e6ed8089f6b9d9a9692a1
Rachele Morino <rachelekm@gmail.com>
2018-05-01 13:53:46 -0400

    travis CI setup

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+  return json.status(200);
+});
