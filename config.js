45302a330c3ce922309eb8e54e169477fdbaf555
Rachele Morino <rachelekm@gmail.com>
2018-05-03 11:10:10 -0400

    initial mock data import and db configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                      'mongodb://localhost/dreamJournalSeedDB';
+//exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+//                      'mongodb://localhost/dreamJournalTestDB';
+exports.PORT = process.env.PORT || 8080;
