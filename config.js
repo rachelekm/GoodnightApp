efd30dc4d0d7c877038f023f1e94174609f73965
Rachele Morino <rachelekm@gmail.com>
2018-05-03 15:02:00 -0400

    create new user router and POST endpoint

--- config.js
+++ config.js
@@ -3,3 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
 //                      'mongodb://localhost/dreamJournalTestDB';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
--- server.js
+++ server.js
@@ -1,13 +1,69 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+
 const app = express();
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
+app.use(morgan('common'));
+
+const { router: newUserRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+app.use('/signup', newUserRouter);
+app.use('/login', authRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
   return json.status(200);
 });
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
  
